9f0a8bd44e829184e486661266ade3dd9c34bf83
Benoit-Monteiro-Da-Silva <benoit.monteirods@gmail.com>
2025-06-14 12:17:27 +0200

    feat(Header): Add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
-//Display this app logo and redirect to the home page when clicked on.
+import { Link } from "react-router"
+import { ROUTES } from "../../constants/routes"
+import pokeballLogo from "../../assets/pokeball.png"
+import pokedexLogo from "../../assets/pokedex.png"
+import style from "./Header.module.css"
 
+//Display this app logo and redirect to the home page when clicked on.
 export function Header() {
     return(
-        <p>Test</p>
+        <header className={style.header}>
+            <Link to={ROUTES.HOME} className={style.link}>
+                <div className={style.pokeball}>
+                    <img src={pokeballLogo} alt=""/>
+                </div>
+                <div className={style.pokedexLogo}>
+                    <img src={pokedexLogo} alt="Pokedex"/>
+                </div>
+            </Link>
+        </header>
     )
 }
